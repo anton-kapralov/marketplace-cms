8f46ae7b0246a948bccfd1610e35ba141d07a8be
Anton Kapralov <a.e.kapralov@gmail.com>
2020-05-06 21:13:48 -0700

    Generate stub Spring Boot project for Author.

---
+++ marketplace-cms-author/src/main/java/kae/demo/marketplacecms/author/MarketplaceCmsAuthorApplication.java
@@ -0,0 +1,13 @@
+package kae.demo.marketplacecms.author;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceCmsAuthorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MarketplaceCmsAuthorApplication.class, args);
+  }
+
+}
---
+++ marketplace-cms-author/src/test/java/kae/demo/marketplacecms/author/MarketplaceCmsAuthorApplicationTests.java
@@ -0,0 +1,13 @@
+package kae.demo.marketplacecms.author;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceCmsAuthorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
